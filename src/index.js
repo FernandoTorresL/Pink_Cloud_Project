5424fc8f7d5fcc00c6d4b1301c254d078ab8860a
Fernando Torres <fertorresmx@gmail.com>
2020-04-17 15:20:40 -0500

    Install Webpack. Fix .babel and HelloWorld component. Run 'npm run build'

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import HelloWorld from './components/HelloWorld.jsx';
+import HelloWorld from './components/HelloWorld';
 
 
 ReactDOM.render(<HelloWorld />, document.getElementById(app));
