1c6ee81f6b8d091d6daca7d6452323effb3c2699
Fer Torres <fertorresmx@gmail.com>
2020-05-03 04:03:23 -0500

    Add html and style to Employees component

--- src/components/Employees.jsx
+++ src/components/Employees.jsx
@@ -1,7 +1,82 @@
 import React from 'react';
+import '../assets/styles/components/Employees.scss';
 
 const Employees = () => (
-  <h1>Employees</h1>
+  <section className='employee'>
+    <div className='employee__nav'>
+      <div>
+        <button className='employee__button'>Agregar</button>
+      </div>
+    </div>
+
+    <div className='employee__table'>
+      <div className='employee__container--gray'>
+
+        <div className='employee__container'>
+
+          <div className='employee__header'>
+            <input type='checkbox' />
+          </div>
+
+          <div className='item' />
+
+          <div className='employee__header--left'>
+            <p>EMPLEADO</p>
+          </div>
+
+          <div className='employee__header--left'>
+            SALARIO
+          </div>
+
+          <div className='employee__header--left'>
+            ESTADO
+          </div>
+
+          <div className='employee__header--left'>
+            ACCIONES
+          </div>
+
+        </div>
+
+      </div>
+
+      <div className='employee__container--white'>
+
+        <div className='employee__container'>
+
+          <div className='employee__header'>
+            <input type='checkbox' />
+          </div>
+
+          <div className='employee__header'>
+            <img src='../images/icono-navmenu.png' alt='Portrait' />
+          </div>
+
+          <div className='employee__item'>
+            <p>Nombre Apellido</p>
+            <p className='employee__job'>Software Enginner</p>
+          </div>
+
+          <div className='employee__item'>
+            <p>2.000 USD</p>
+            <p className='employee__job'>full time</p>
+          </div>
+
+          <div className='employee__item'>
+            <p>Activo</p>
+          </div>
+
+          <div className='employee__header--left'>
+            <img src='../images/editar-employee.png' alt='Portrait' />
+            <img src='../images/borrar-employee.png' alt='Portrait' />
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
+  </section>
 );
 
 export default Employees;
