018c60859872d4dc92194f7b0fb98559f50abe6c
Fer Torres <fertorresmx@gmail.com>
2020-05-14 11:56:04 -0500

    Try a solution to a challenge

--- backend/index.js
+++ backend/index.js
@@ -12,6 +12,12 @@ app.get('/json', function(req, res) {
   res.json({ hello: 'world' });
 });
 
+app.get('/anyo-bisiesto-checker', function (req, res) {
+  const {anyo} = req.query;
+  const anyoBisiesto = (anyo % 4 === 0 && anyo % 100 !== 0) || anyo % 400 === 0;
+  res.send(`El año ${anyo} ${anyoBisiesto ? 'es bisiesto' : 'no es bisiesto'}.`);
+});
+
 app.listen(config.port, function() {
   console.log(`Listening http://localhost:${config.port}`);
 });
