55c4a0db1c29b4568c490c7acf3044439304acd4
Fer Torres <fertorresmx@gmail.com>
2020-05-14 18:41:13 -0500

    Implement Boom

--- backend/index.js
+++ backend/index.js
@@ -7,15 +7,24 @@ const employeesApi = require('./routes/employees.js');
 
 const {
   logErrors,
+  wrapErrors,
   errorHandler
 } = require('./utils/middleware/errorHandlers.js');
+
+const notFoundHandler = require('./utils/middleware/notFoundHandler');
 
 // body parser
 app.use(express.json());
 
+// routes
 employeesApi(app);
 
+// Catch 404
+app.use(notFoundHandler);
+
+// Errors middleware
 app.use(logErrors);
+app.use(wrapErrors);
 app.use(errorHandler);
 
 app.listen(config.port, function() {
