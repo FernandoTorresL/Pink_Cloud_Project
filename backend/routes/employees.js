8c3f5759fff3118e81c56410564e43fc9124f0bc
Fer Torres <fertorresmx@gmail.com>
2020-05-14 14:05:28 -0500

    Implement other methods CRUD

--- backend/routes/employees.js
+++ backend/routes/employees.js
@@ -10,13 +10,66 @@ function employeesApi(app) {
       const employees = await Promise.resolve(employeesMock);
 
       res.status(200).json({
+        message: 'All employees listed',
         data: employees,
-        message: 'employees listed',
       });
     } catch(err) {
       next(err);
     }
   });
+
+  router.get('/:employeeId', async function(req, res, next) {
+    try {
+      const employee = await Promise.resolve(employeesMock[0]);
+
+      res.status(200).json({
+        message: '1 employee retrieved',
+        data: employee,
+      });
+    } catch(err) {
+      next(err);
+    }
+  });
+
+  router.post('/', async function(req, res, next) {
+    try {
+      const createEmployeeId = await Promise.resolve(employeesMock[0].id);
+
+      res.status(201).json({
+        message: '1 employee created',
+        data: createEmployeeId,
+      });
+    } catch(err) {
+      next(err);
+    }
+  });
+
+  router.put('/:employeeId', async function(req, res, next) {
+    try {
+      const updatedEmployeeId = await Promise.resolve(employeesMock[0].id);
+
+      res.status(200).json({
+        message: '1 employee updated',
+        data: updatedEmployeeId,
+      });
+    } catch(err) {
+      next(err);
+    }
+  });
+
+  router.delete('/:employeeId', async function(req, res, next) {
+    try {
+      const deletedEmployeeId = await Promise.resolve(employeesMock[0].id);
+
+      res.status(200).json({
+        message: '1 employee deleted',
+        data: deletedEmployeeId,
+      });
+    } catch(err) {
+      next(err);
+    }
+  });
+
 }
 
 module.exports = employeesApi;
