4e525b5b73fe23059951998e37e5b0324858477c
Fer Torres <fertorresmx@gmail.com>
2020-05-15 04:44:55 -0500

    Add first routes

---
+++ frontend/src/routes/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from '../containers/Home';
+
+const App = () => (
+<BrowserRouter>
+    {/* <Layout> */}
+      <Switch>
+        <Route exact path='/' component={Home} />
+        {/* <Route exact path='/login' component={Login} />
+        <Route exact path='/register' component={Register} />
+        <Route component={NotFound} /> */}
+      </Switch>
+    {/* </Layout> */}
+  </BrowserRouter>
+);
+
+export default App;
